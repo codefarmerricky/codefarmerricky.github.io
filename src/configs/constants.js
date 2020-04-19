1f6f70c60f985bae7a1c298f7edd16c6c10d7f07
codefarmerricky <33510531+codefarmerricky@users.noreply.github.com>
2020-04-19 08:47:07 +0800

    change configuration

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -3,11 +3,11 @@ export const SITE_NAME = 'Ricky Blog'; // 网站标题
 export const SITE_ADDRESS = ''; // 网站地址，可为空
 export const SITE_MOTTO = '屁屁'; // 一句段子
 export const OWNER = '"codefarmerricky"'; // 你github name
-export const PROJECT = '"blog"'; // 你issue项目名
+export const PROJECT = '"codefarmerricky.github.io"'; // 你issue项目名
 // because of github protect privacy, the token compile with a funciton to encrypted；
 // 由于github安全保护规则，token不能暴露到仓库中，但又因为我们申请的token只是一个只读token，所以这里使用了简单函数进行了对称加解密绕开规则；
 export const TOKEN = 'clÇnmjiefÇildjkdhogdikmq';
-export const GITHUB_URL = 'https://codefarmerricky.github.io/blog/';
+export const GITHUB_URL = 'https://codefarmerricky.github.io';
 /* 博客展示相关 */
 export const PAGE_SIZE = 10;
 // change the picture url, change the color;
